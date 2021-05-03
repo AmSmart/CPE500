08c6cd899de8c4447e3d7cc20d863a64837e879f
Adebiyi Emmanuel <smartemma03@gmail.com>
2021-05-04 00:47:10 +0100

    2nd semester updates

--- CPE500/Startup.cs
+++ CPE500/Startup.cs
@@ -23,7 +23,7 @@ namespace CPE500
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddRazorPages();                
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
